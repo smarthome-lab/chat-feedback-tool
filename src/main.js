51387098b3a14043a9d5df155c0178d07e6362df
ZeroSoulEater <t1@purzner.de>
2018-05-12 16:42:48 +0200

    [ADD] Make components useable everywhere

--- src/main.js
+++ src/main.js
@@ -5,14 +5,24 @@ import App from './App'
 import router from './router'
 import SuiVue from 'semantic-ui-vue'
 import 'semantic-ui-css/semantic.min.css'
+import Vuex from 'vuex'
+import 'es6-promise/auto'
+import { store } from './store/index';
+import Nav from './components/nav'
+import Pagination from './components/pagination'
 
 Vue.config.productionTip = false
 Vue.use(SuiVue)
+Vue.component('nav-bar', Nav);
+Vue.component('pagination', Pagination);
+
+
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
   components: { App },
-  template: '<App/>'
+  template: '<App/>',
+  store
 })
