6c5fd23c0ea0cea2226736ebfa4a31c2b315e2f0
ZeroSoulEater <t1@purzner.de>
2018-05-12 16:43:23 +0200

    [MOD] some things

---
+++ src/screens/PageNotFound.vue
@@ -0,0 +1,21 @@
+<template>
+  <p>
+    404 - Seite nicht gefunden!
+  </p>
+</template>
+
+<script>
+  export default {
+    name: 'PageNotFound',
+    data () {
+      return {
+        msg: 'Welcome to Your Vue.js App'
+      }
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
