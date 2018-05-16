4ced83f5b3098c60790e38d4de52ad5025dce621
ZeroSoulEater <t1@purzner.de>
2018-05-16 23:37:41 +0200

    [FIX] Wrong backenend

--- src/feathers-client.js
+++ src/feathers-client.js
@@ -3,7 +3,7 @@ import socketio from '@feathersjs/socketio-client'
 import auth from '@feathersjs/authentication-client'
 import io from 'socket.io-client'
 
-const connection = process.env["NODE_ENV"]==='production' ? process.env["https://hsc-backend.herokuapp.com"]: 'http://localhost:3030'
+const connection = process.env["NODE_ENV"]==='production' ? "https://hsc-backend.herokuapp.com": 'http://localhost:3030'
 const socket = io(connection, {transports: ['websocket']})
 const feathersClient = feathers()
   .configure(socketio(socket))
