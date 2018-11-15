c767fda03857918a3773fc8baef6678adaba6262
Icheler <ingo.scheler@online.de>
2018-11-15 17:41:53 +0100

    Update feathers-client.js

--- src/feathers-client.js
+++ src/feathers-client.js
@@ -3,7 +3,7 @@ import socketio from '@feathersjs/socketio-client'
 import auth from '@feathersjs/authentication-client'
 import io from 'socket.io-client'
 
-const connection = process.env["NODE_ENV"]==='production' ? "https://hsc-backend.herokuapp.com": "https://hsc-backend-staging.herokuapp.com"
+const connection = process.env["NODE_ENV"]==='production' ? "https://hsc-backend.herokuapp.com": "localhost:3030"
 const socket = io(connection, {transports: ['websocket']})
 const feathersClient = feathers()
   .configure(socketio(socket))
