59517c5e49e9a97f729e85283515d58df41e42a7
Christian <Totenfluch@users.noreply.github.com>
2018-11-15 21:07:06 +0100

    Request for Deployment (#8)
    * created dummy useroverview path and site
    
    * create userslist
    
    * Add pagination, better search implementation
    
    * better search, sort ascending in db query
    
    * add search via hsid, comment, remove unnecessary code
    
    * added Navigation to Feedback and UserOverview
    
    * Add pagination at top of list
    
    * remove margin top
    
    * add id to select
    
    * added link to table, task complete
    
    * Update feathers-client.js
    
    * Changed URL with http to localhost
    
    * changed from double to single quotes
    
    * Fixed a few Lining Errors and changed Link from id to mail
    
    * Added filter to UserOverview (#7)
    
    * Added filter to UserOverview
    
    * reworked filter, fixed linting
    
    * Added filter to UserOverview
    
    * revert url change
    
    * bow to req

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,7 @@ import HelloWorld from '@/screens/HelloWorld'
 import Login from '@/screens/Login'
 import PageNotFound from '@/screens/PageNotFound'
 import Feedback from '@/screens/Feedback'
+import UserOverview from '@/screens/UserOverview'
 import {feathersClient} from '../feathers-client'
 import {store} from '../store/index'
 
@@ -28,10 +29,16 @@ const router = new Router({
       meta: {requiresAuth: true}
     },
     {
+      path: '/users',
+      name: 'Benutzeruebersicht',
+      component: UserOverview,
+      meta: {requiresAuth: true}
+    },
+    {
       path: '*',
       name: '404',
       component: PageNotFound
-    },
+    }
   ]
 })
 
@@ -45,7 +52,7 @@ router.beforeEach(async (to, from, next) => {
     if (user === null || user === {} || user === undefined) {
       let c = await checkAuth()
       // if the user is still loged in show page else redirect to login
-      if(c) {
+      if (c) {
         next()
         return
       }
@@ -58,8 +65,7 @@ router.beforeEach(async (to, from, next) => {
   }
 })
 
-
-async function checkAuth() {
+async function checkAuth () {
   return await feathersClient.authenticate().then(async (response) => {
     return await feathersClient.passport.verifyJWT(response.accessToken).then(async (u) => {
       return await feathersClient.service('users').get(u.userId).then((u) => {
@@ -75,6 +81,5 @@ async function checkAuth() {
     return Promise.resolve(false)
   })
 }
-
 
 export default router
