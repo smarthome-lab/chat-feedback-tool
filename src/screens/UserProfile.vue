c059fd13f99c6a15f053c44db14e78378686f204
LouisaSchirmer <Louisa.Schirmer@gmx.de>
2018-11-17 14:22:06 +0100

    Insert query

--- src/screens/UserProfile.vue
+++ src/screens/UserProfile.vue
@@ -2,9 +2,11 @@
 <div id="userprofile">
   <nav-bar></nav-bar>
   <div class="content">
-  <h1>Benutzerprofil für {{benutzername}}</h1>
-  <table>
-    <tr>
+  <h1>Benutzerprofil für {{displayedUser.prename}}</h1>
+
+  <table class="ui celled table">
+    <thead class="bottom aligned">
+      <tr>
       <th>Vorname</th>
       <th>Name</th>
       <th>E-Mail</th>
@@ -18,22 +20,24 @@
       <th>Ablaufdatum der Verifizerung</th>
       <th>Zuletzt online</th>
       <th>Ist online</th>
-    </tr>
-    <tr>
-      <th>Test</th>
-      <th>Test</th>
-      <th>Test</th>
-      <th>{{kennung}}</th>
-      <th>Test</th>
-      <th>Test</th>
-      <th>Test</th>
-      <th>Test</th>
-      <th>Test</th>
-      <th>Test</th>
-      <th>Test</th>
-      <th>Test</th>
-      <th>Test</th>
-    </tr>
+    </tr></thead>
+    <tbody>
+      <tr>
+        <td>{{displayedUser.prename}}</td>
+        <td>{{displayedUser.lastname}}</td>
+        <td>{{displayedUser.email}}</td>
+        <td>{{displayedUser.hsid}}</td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+      </tr>
+    </tbody>
   </table>
 </div>
 </div>
@@ -45,13 +49,41 @@ import {mapGetters, mapMutations} from 'vuex'
 
 export default {
   name: 'userprofile',
+  
   data () {
     return {
-      benutzername: this.$route.params.id,
-      kennung: this.$route.params.id
+      displayedUser: {}, 
+
     }
-  }
+  },
+  created(){
+      console.log('created');
+      this.loadData();
+    } , 
+  methods:{
+    loadData(){
+      console.log('LoadDAta');
+        feathersClient.passport.verifyJWT(response.accessToken).then((u)=> {
+          feathersClient.service('users').get(u.userId).then((user) => {
+            if(user.userId === this.$route.params.id){
+               console.log(JSON.stringify(user));
+                 this.displayedUser = user;
+            }else{
+              console.log('something wrong with request');
+            }
+
+          })
+        })
+
+    },
+
+    
+
+
+  } 
 }
+
+
 </script>
 
 <style>
