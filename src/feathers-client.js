06ff1b32b276014d072cda67452df447674b9046
Christian <Totenfluch@users.noreply.github.com>
2018-11-22 21:46:20 +0100

    Update feathers-client.js

--- src/feathers-client.js
+++ src/feathers-client.js
@@ -3,7 +3,7 @@ import socketio from '@feathersjs/socketio-client'
 import auth from '@feathersjs/authentication-client'
 import io from 'socket.io-client'
 
-const connection = 'http://localhost:3030'
+const connection = 'https://hsc-backend.herokuapp.com'
 const socket = io(connection, { transports: ['websocket'] })
 const feathersClient = feathers()
   .configure(socketio(socket))
